4e1c70c878526b2bf546686e825cbb4560a530a1
daniscodes <dan97@bell.net>
2023-05-24 17:39:07 -0400

    implement footer component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { Suspense } from 'react';
 import { Loader } from '@react-three/drei';
 import './index.css';
+import Footer from './components/Footer';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
 <>
 <Suspense fallback={null}>
-  <>
-  </>
+  <Footer />
 </Suspense>
 <Loader />
 </>
